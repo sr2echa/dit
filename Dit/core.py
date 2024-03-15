97ed299de3ac42afcfb68ae732de363e9df7f31d
sr2echa <65058816+sr2echa@users.noreply.github.com>
2024-03-15 17:01:41 +0530

    init

---
+++ Dit/core.py
@@ -0,0 +1,2 @@
+def helloworld():
+    pass
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Dit',
+    version='0.0.1',
+    url='https://github.com/sr2echa/dit', 
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
